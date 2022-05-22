ca07a2e91da3822a700a66ccfdf9a52cc1f9b92f
Sumaita Tanjim Hridy <86717312+Sumaita-hridy@users.noreply.github.com>
2022-05-22 14:07:01 +0600

    Add files via upload
    simple project only show and hide password in react website

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+function App() {
+
+  const [pwd, setPwd] = useState('');
+  const [isRevealPwd, setIsRevealPwd] = useState(false);
+
+  return (
+    <div className="container">
+      <h3>Show and hide password in React </h3>
+
+      <div className="pwd-container">
+        <input
+          name="pwd"
+          placeholder="Enter Password"
+          type={isRevealPwd ? "text" : "password"}
+          value={pwd}
+          onChange={e => setPwd(e.target.value)}
+        />
+        <button  
+          onClick={() => setIsRevealPwd(prevState => !prevState)}
+        >
+        {isRevealPwd ? "Hide password" : "Show password"}
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
